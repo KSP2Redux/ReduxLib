b8abbe0144ca63d6490567ea8a793a081dbb219f
Lexi Allen <arallen3660@gmail.com>
2024-11-23 13:33:33 -0500

    Port over Space Warp's messaging API

---
+++ Runtime/ReduxLib/Messaging/MessageBusBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace ReduxLib.Messaging;
+
+/// <summary>
+/// A base class for MessageBus instances.
+/// </summary>
+[PublicAPI]
+public abstract class MessageBusBase
+{
+    /// <summary>
+    /// The name of the MessageBus.
+    /// </summary>
+    public string Name { get; internal set; }
+    internal abstract IReadOnlyList<Delegate> Handlers { get; }
+    internal abstract void RemoveHandlerAt(int index);
+}
