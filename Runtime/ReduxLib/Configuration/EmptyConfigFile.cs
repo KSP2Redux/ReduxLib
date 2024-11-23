97d83a173aea77e34ffb8dbb7923f253683832c6
Lexi Allen <arallen3660@gmail.com>
2024-11-23 13:32:55 -0500

    Add empty config file

---
+++ Runtime/ReduxLib/Configuration/EmptyConfigFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReduxLib.Configuration;
+
+public class EmptyConfigFile : IConfigFile
+{
+    public void Save()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IConfigEntry this[string section, string key] => throw new KeyNotFoundException($"{section}, {key}");
+
+    public IConfigEntry Bind<T>(string section, string key, T? defaultValue = default, string description = "",
+        IValueConstraint? constraint = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IReadOnlyList<string> Sections => Array.Empty<string>();
+
+    public IReadOnlyList<string> this[string section] => Array.Empty<string>();
+}
